dc708932ecb1df6f1f4ffcf4c4b4e536d89cbcaf
karlh07 <karlh07@gmail.com>
2016-12-02 16:08:32 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # unity-symlink-utility
+
 A small extension that adds a menu item to add folders as symlinks in Unity.
+
+## Quick Warning from Unity
+
+> Using symlinks in Unity projects may cause your project to become corrupted if you create multiple references to the same asset, use recursive symlinks or use symlinks to share assets between projects used with different versions of Unity. Make sure you know what you are doing.
+
+Basically just don't create multiple symlinks pointing to the same folder, and don't make symlinks inside symlinks.
+
+## Quick Start
+
+![](menuitem.png)
+
+- Open the menu item `Assets > Create > Folder (Symlink)`
+- Choose the directory source
+
+New symlinks are placed in the selected directory, named to match the source folder.
---
+++ SymlinkUtility.cs
@@ -0,0 +1,110 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Parabox
+{
+	/**
+	 *	An editor utility for easily creating symlinks in your project.
+	 *
+	 *	Adds a Menu item under `Assets/Create/Folder (Symlink)`, and 
+	 *	draws a small indicator in the Project view for folders that are
+	 *	symlinks.
+	 */
+	[InitializeOnLoad]
+	public static class SymlinkUtility
+	{
+		// FileAttributes that match a junction folder.
+		const FileAttributes FOLDER_SYMLINK_ATTRIBS = FileAttributes.Directory | FileAttributes.ReparsePoint;
+
+		// Style used to draw the symlink indicator in the project view.
+		private static GUIStyle _symlinkMarkerStyle = null;
+		private static GUIStyle symlinkMarkerStyle
+		{
+			get
+			{
+				if(_symlinkMarkerStyle == null)
+				{
+					_symlinkMarkerStyle = new GUIStyle(EditorStyles.label);
+					_symlinkMarkerStyle.normal.textColor = new Color(.2f, .8f, .2f, .8f);
+					_symlinkMarkerStyle.alignment = TextAnchor.MiddleRight;
+				}
+				return _symlinkMarkerStyle;
+			}
+		}
+
+		/**
+		 *	Static constructor subscribes to projectWindowItemOnGUI delegate.
+		 */
+		static SymlinkUtility()
+		{
+			EditorApplication.projectWindowItemOnGUI += OnProjectWindowItemGUI;
+		}
+
+		/**
+		 *	Draw a little indicator if folder is a symlink
+		 */
+		private static void OnProjectWindowItemGUI(string guid, Rect r)
+		{
+			string path = AssetDatabase.GUIDToAssetPath(guid);
+
+			FileAttributes attribs = File.GetAttributes(path);
+
+			if( (attribs & FOLDER_SYMLINK_ATTRIBS) == FOLDER_SYMLINK_ATTRIBS )
+				GUI.Label(r, "<=>", symlinkMarkerStyle);
+		}
+
+		/**
+		 *	Add a menu item in the Assets/Create category to add symlinks to directories.
+		 */
+		[MenuItem("Assets/Create/Folder (Symlink)", false, 20)]
+		static void DoTheSymlink()
+		{
+			string sourceFolderPath = EditorUtility.OpenFolderPanel("Select Folder Source", "", "");
+
+			if( sourceFolderPath.Contains(Application.dataPath) )
+			{
+				UnityEngine.Debug.LogWarning("Cannot create a symlink to folder in your project!");
+				return;
+			}
+
+			string sourceFolderName = sourceFolderPath.Split(new char[] { '/', '\\' }).LastOrDefault();
+
+			if( string.IsNullOrEmpty(sourceFolderName) )
+			{
+				UnityEngine.Debug.LogWarning("Couldn't deduce the folder name?");
+				return;
+			}
+
+			Object uobject = Selection.activeObject;
+
+			string targetPath = uobject != null ? AssetDatabase.GetAssetPath(uobject) : "";
+
+			if(string.IsNullOrEmpty(targetPath))	
+				targetPath = "Assets";
+
+			FileAttributes attribs = File.GetAttributes(targetPath);
+
+			if( (attribs & FileAttributes.Directory) != FileAttributes.Directory )
+				targetPath = Path.GetDirectoryName(targetPath);
+
+			targetPath = string.Format("{0}/{1}", targetPath, sourceFolderName);
+
+			if( Directory.Exists(targetPath) )	
+			{
+				UnityEngine.Debug.LogWarning(string.Format("A folder already exists at this location, aborting link.\n{0} -> {1}", sourceFolderPath, targetPath));
+				return;
+			}
+
+	#if UNITY_EDITOR_WIN
+			Process.Start("CMD.exe", string.Format("/C mklink /J \"{0}\" \"{1}\"", targetPath, sourceFolderPath));
+			UnityEngine.Debug.Log(string.Format("Created symlink: {0} <=> {1}", targetPath, sourceFolderPath));
+	#elif UNITY_EDITOR_OSX || UNITY_EDITOR_LINUX
+			// @todo
+	#endif
+		}
+	}
+}
